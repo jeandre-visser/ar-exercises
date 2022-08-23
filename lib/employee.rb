e941efecd6e476d09bb5c6bc62408fc0df561c1a
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-23 00:47:59 +0000

    Add validators

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,17 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  # Employees must always have a first name present
+  # Employees must always have a last name present
+  validates :first_name, :last_name, presence: true
+
+  # Employees have a hourly_rate that is a number (integer) between 40 and 200
+  validates :hourly_rate, numericality: {
+    only_integer: true,
+    greater_than: 40, 
+    less_than: 200
+  }
+
+  # Employees must always have a store that they belong to
+  validates :store_id, presence: true
 end
